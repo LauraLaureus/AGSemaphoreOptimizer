fd8081b7531658729f81e50b76d2462f68abc5af
LauraLaureus <l4depende@gmail.com>
2015-12-01 19:22:51 +0000

    Added the option to reset the simulator

--- computeFitness.m
+++ computeFitness.m
@@ -2,6 +2,9 @@ function [fitnesses]=calculaFitness(A,simulator)
 fitnesses = zeros(size(A,3),1); %Vector columnaaa
 
 for i=1:size(A,3)
+    %disp('Tiempo de simulacion');
+    %tic;
     fitnesses(i) = simulator.simulate(A(:,:,i));
+    %toc;
 end
 end
